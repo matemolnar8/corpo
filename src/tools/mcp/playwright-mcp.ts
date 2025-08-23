9745e3874ce69fda8d5fa7d6b7e0ef898525acfc
matemolnar8 <doglotteger@gmail.com>
2025-08-23 22:47:58 +0200

    Revert "Remove evaluate"
    This reverts commit 0fee5a012754e396acea5533842a26c7c6e42308.

--- src/tools/mcp/playwright-mcp.ts
+++ src/tools/mcp/playwright-mcp.ts
@@ -27,7 +27,7 @@ export class PlaywrightMCP {
     "browser_select_option",
     "browser_tab_list",
     "browser_tab_select",
-    // "browser_evaluate",
+    "browser_evaluate",
     // "browser_snapshot",
   ];
 
